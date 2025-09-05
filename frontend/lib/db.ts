76441dd2aab9a1e33a583ef37245fcab893c52e6
Ajay <ajayrajnegi111@gmail.com>
2025-09-05 20:46:47 +0530

    fuck

--- frontend/lib/db.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "./generated/prisma";
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-globalForPrisma.prisma = prisma;
